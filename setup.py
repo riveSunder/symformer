e054a5959c6665318fb4da934305af8d0aa2c4dc
RiveSunder <rivesunder@protonmail.com>
2022-09-27 11:31:04 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(\
+        name="symformer", \
+        version = "0.0", \
+        description = "Symformer by Vastl et al.", \
+        packages=find_packages(), \
+    )
